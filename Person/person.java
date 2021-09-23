9f44ea5dc9fc7e8d29bfa096bd7ee4eab8b62afe
Arianna Zhang <59743164+ariannazhangg@users.noreply.github.com>
2021-09-23 10:01:41 -0700

    Add files via upload

---
+++ Person/person.java
@@ -0,0 +1,12 @@
+class person {
+    private String name;
+    private String age;
+    private boolean alive;
+    
+    public person(){
+        name = "Elaine";
+        age = "100";
+        alive = true;
+    
+    }
+}
